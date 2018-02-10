82ee8ddef97db4f2a69f795f61b0dc657545c358
Wilhelm Grosz <wilhelm.grosz@iquestgroup.com>
2018-02-10 20:48:46 +0200

    updates app.module with the corect imports/declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,35 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule } from '@ionic/storage';
+import { HttpModule } from '@angular/http';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from '../pages/dashboard/dashboard.component';
+import { EntriesModule } from '../pages/entries/entries.module';
+import { EntriesService } from '../services/entries.service';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    EntriesModule,
+    IonicModule.forRoot(AppComponent),
+    IonicStorageModule.forRoot(),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    AppComponent,
+    DashboardComponent
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    EntriesService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
